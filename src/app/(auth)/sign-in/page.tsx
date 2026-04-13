f5dfd47fd3a74da46670d1a3a334269f2531e5b0
Chandra Shekhar <banothuchandu77@gmail.com>
2026-04-14 00:03:00 +0530

    style: update SignInForm component with new branding and messaging

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -92,13 +92,13 @@ export default function SignInForm() {
     }
 
 
-    return (<div className="flex justify-center items-center min-h-screen bg-gray-800">
-        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+    return (<div className="flex justify-center items-center min-h-screen bg-black">
+        <div className="w-full max-w-md p-8 space-x-0 space-y-8 bg-white rounded-lg shadow-md">
             <div className="text-center">
                 <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-                    Welcome Back to True Feedback
+                    Welcome Back to SnapShop
                 </h1>
-                <p className="mb-4">Sign in to continue your secret conversations</p>
+                <p className="mb-4">Sign in and Start Shopping 🛍️</p>
             </div>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
@@ -144,8 +144,12 @@ export default function SignInForm() {
                 </p>
             </div>
             <div className="text-center mt-4">
-                <Link href="/forgot-password" className="text-blue-600 hover:text-blue-800">Forgot Password</Link>
-
+<p>
+                    Did you forgot password?{' '}
+                    <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+                        Forgot password
+                    </Link>
+                </p>
             </div>
         </div>
     </div>
