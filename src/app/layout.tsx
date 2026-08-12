fcd10c308267897ab8f602d66738d7978b31239e
Chandra Shekhar <banothuchandu77@gmail.com>
2026-08-12 23:33:51 +0530

    refactor: remove Provider component from RootLayout and adjust layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
-import Provider from "./provider"
 import { Toaster } from "../components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -13,10 +12,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider>
           {children}
           <Toaster />
-        </Provider>
       </body>
     </html>
   )
